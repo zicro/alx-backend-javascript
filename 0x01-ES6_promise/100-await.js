92f33194ff0f0b4847c929fef6f98c14f74778b3
zicro <larhnimikit@gmail.com>
2024-03-05 08:28:45 +0000

    acheive promises..

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,17 @@
+import { createUser, uploadPhoto } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    return {
+      photo,
+      user,
+    };
+  } catch (error) {
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
